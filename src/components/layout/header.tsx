786e94cdbaa9ff4210b1c44bd0dd28c03aaddfab
info tripjoy <tripjoyinfo@gmail.com>
2026-01-14 18:44:56 +0000

    headter colour should black

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -56,7 +56,7 @@ export default function Header() {
   )
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-black text-white">
       <div className="container flex h-16 items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
           <Logo />
